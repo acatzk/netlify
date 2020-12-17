aa9d0554d2a3830c80442f958c94dd2dab72b5bd
Joshua Galit <joshuaimalay@gmail.com>
2020-12-17 10:19:57 +0800

    Added darkest and hover black colors of netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {}
+      colors: {
+        'grey-darkest': '#0e1e25',
+        'black-20': 'rgba(0, 0, 0, 0.2)',
+      }
     },
   },
   variants: {
