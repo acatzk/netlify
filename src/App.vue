139191ef4d883a85d6b50aa17df6592e8812c035
Joshua Galit <joshuaimalay@gmail.com>
2020-12-26 08:59:05 +0800

    main and footer netlify design - DONE

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="font-sans leading-tight text-grey-darkest">
+  <div id="app" class="font-sans leading-tight text-gray-700">
     <header class="bg-grey-darkest antialiased pb-32">
       <div class="container max-w-7xl mx-auto px-6">
         <div class="flex items-center justify-between pt-4 pb-2 border-b-2 border-gray-700">
@@ -68,7 +68,114 @@
           </div>
         </div>
       </div>
+      <div class="relative flex flex-wrap -mx-6">
+        <div class="sticky w-full lg:w-xs flex-none px-6">
+          <div class="bg-white border rounded overflow-hidden">
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-bold text-black px-4 py-3 border-teal-500">General</a>
+            </div>            
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition bg-gray-50 block text-sm font-semibold text-black pl-6 pr-4 py-3">Site details</a>
+            </div>             
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition bg-gray-50 block text-sm font-semibold text-gray-500 pl-6 pr-4 py-3">Site Administrators</a>
+            </div>   
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition bg-gray-50 block text-sm font-semibold text-gray-500 pl-6 pr-4 py-3">Danger zone</a>
+            </div>           
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Build & Deploy</a>
+            </div> 
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Domain management</a>
+            </div>            
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Functions</a>
+            </div>       
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Identity</a>
+            </div>       
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Forms</a>
+            </div>           
+            <div class="border-b">
+              <a href="#" class="hover:bg-gray-100 transition block border-l-2 border-transparent font-semibold text-gray-500 px-4 py-3">Access control</a>
+            </div> 
+          </div>
+        </div>  
+        <div class="flex-1 px-6">
+          <div class="mb-6 mt-6 lg:mt-0">
+            <h1 class="text-2xl font-bold mb-3 text-gray-800 leading-snug">Site details</h1>
+            <p class="text-gray-500 font-semibold">General information about your site</p>
+          </div>
+          <div class="space-y-6">
+            <div v-for="i in 5" :key="i">
+              <div class="justify-between bg-white rounded-lg shadow p-6">
+                <h3 class="text-2xl font-semibold mb-2 border-b border-gray-200 pb-3">Site details</h3>
+                <table class="w-full -mx-6">
+                  <tr>
+                    <td class="w-1/4 font-semibold text-gray-500 px-6 py-2">Site name:</td>
+                    <td class="w-3/4 font-bold px-6 py-2">
+                      <a href="#" class="border-b text-gray-600 hover:border-black">eleted-stonebraker-7089a</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="bg-gray-100 w-1/4 font-semibold text-gray-500 px-6 py-2">Owner:</td>
+                    <td class="bg-gray-100 w-3/4 font-bold px-6 py-2">
+                      Joshua Gali
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-1/4 font-semibold text-gray-500 px-6 py-2">Repository:</td>
+                    <td class="w-3/4 font-bold px-6 py-2">
+                      <a href="#" class="border-b text-gray-600 hover:border-black">github.com/acatzk/vue-portfolio</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="bg-gray-100 w-1/4 font-semibold text-gray-500 px-6 py-2">API ID:</td>
+                    <td class="bg-gray-100 w-3/4 font-bold px-6 py-2">
+                      4299d0e-b846-4039-bfb5-5b3a1cd68a3b 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="w-1/4 font-semibold text-gray-500 px-6 py-2">Created:</td>
+                    <td class="w-3/4 font-bold px-6 py-2">
+                      December 26, 2020
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="bg-gray-100 w-1/4 font-semibold text-gray-500 px-6 py-2">Last update:</td>
+                    <td class="bg-gray-100 w-3/4 font-bold px-6 py-2">
+                      December 26, 2020
+                    </td>
+                  </tr>
+                </table>
+                <div class="mt-6">
+                  <button class="border shadow px-4 py-2 rounded font-semibold">
+                    Change name site
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
+    <footer class="mt-10 bg-grey-darkest">
+      <div class="container max-w-7xl mx-auto px-6 py-8">
+        <div>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">Docs</a>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">Pricing</a>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">Support</a>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">News</a>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">News</a>
+          <a href="#" class="inline-block font-semibold text-gray-500 mr-2 mb-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white transition">Terms</a>
+        </div>
+        <div>
+          <p class="text-sm text-gray-500">&copy; Netlify 2020</p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
